7b13d7542b55c03636781f99a4e72e45d93b0d41
Benjamin Donovan <btdonovan@gmail.com>
2014-01-29 19:04:49 -0700

    added problem set 0

---
+++ ps0/ps0.py
@@ -0,0 +1,9 @@
+# Problem Set 0
+# Name: Benjamin Donovan
+# Collaborators: None
+# Time spent: 0:15
+print "Enter your date of birth:"
+bday = raw_input("**")
+print "Enter your last name:"
+lname = raw_input("**")
+print bday, lname
