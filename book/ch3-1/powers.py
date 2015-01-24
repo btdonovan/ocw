7f7de73705323fd2d9bc040b12336af13105e016
Benjamin Donovan <btdonovan@gmail.com>
2015-01-24 15:25:15 -0700

    stuff

--- book/ch3-1/powers.py
+++ book/ch3-1/powers.py
@@ -1,15 +1,34 @@
-#! /usr/bin/python
+#! /usr/bin/env python3
 
-# Write a program that asks the user to enter an integer and prints two integers, root and pwr, such that 0 < pwr < 6 and root^pwr is equal to the integer entered by the user.  If no such pair of integers exists, it should print a message to that effect.
+def finger(number):
+    """
+    Write a program that asks the user to enter an integer and prints two integers, root and pwr, such that 0 < pwr < 6 and root^pwr is equal to the integer entered by the user.  If no such pair of integers exists, it should print a message to that effect.
+    
+    Note that this specification will include the number 1 as a power.  A number raised to the power of one will equal itself.  This program will always produce a valid output so the code required to print a message saying that no such integers exist will never run.  Bad specification.
+    """
+    root = abs(number)
+    while abs(root) <= abs(number):
+            pwr = 1
+            while pwr < 6:
+                    if root**pwr == number:
+                            print(root, 'raised to the power of', pwr, 'equals', str(number) + '.')
+                    pwr += 1
+            root -= 1
 
-number = int(raw_input('Enter a whole number: '))
-root = abs(number)
-guess = 0
-while abs(root) <= abs(number):
-	pwr = 1
-	while pwr < 6:
-		if root**pwr == number:
-			print root, 'raised to the power of', pwr, 'equals', str(number) + '.'
-		pwr += 1
-	root -= 1
-# Note that this specification will include the number 1 as a power.  A number raised to the power of one will equal itself.  This program will always produce a valid output so the code required to print a message saying that no such integers exist will never run.  Bad specification.
+def pCube(x):
+    """Find the cube root of a perfect cube."""
+    ans = 0
+    while ans**3 < abs(x):
+        ans = ans + 1
+    if ans**3 != abs(x):
+        print(x, 'is not a perfect cube')
+    else:
+        if x < 0:
+            ans = -ans
+        print('Cube root of ' + str(x) + ' is ' + str(ans))
+
+def testMax(max):
+    i = 0
+    while i < max:
+        i = i + 1
+    print(i)
